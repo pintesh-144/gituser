479abf6bea05e1edba346f3eacb88f1f3f6902ae
root <root@ip-172-31-11-59.ap-south-1.compute.internal>
2020-05-16 17:20:49 +0000

    guess game commit

---
+++ guessgame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+num = 45
+guess_number = int(input("guess  a number"))
+if guess_number == num :
+    print("you win ")
+else :
+    if guess_number > num :
+        print("too high ")
+    else:
+        print("too low")
+
+    
+ 
